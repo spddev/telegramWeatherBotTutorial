da0695e0551689fbd54023038615105cc5fcf5ac
SpdDev <spd@tutanota.com>
2021-06-09 15:18:03 +0400

    First commit

--- main_weather_tg_bot.py
+++ main_weather_tg_bot.py
@@ -14,5 +14,56 @@ async def start_command(message: types.Message):
     await message.reply("Привет! Напиши мне название города и я пришлю сводку погоды!")
 
 
+@dp.message_handler()
+async def get_weather(message: types.Message):
+    # Создадим словарь для описаний погоды того или иного города в виде смайлов
+    code_to_smile = {
+        "Clear": "Ясно \U00002600",
+        "Clouds": "Облачно \U00002601",
+        "Rain": "Дождь \U00002614",
+        "Drizzle": "Дождь \U00002614",
+        "Thunderstorm": "Гроза \U000026A1",
+        "Snow": "Снег \U0001F328",
+        "Mist": "Туман \U0001F32B",
+    }
+    try:
+        r = requests.get(
+            f"http://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_weather_api_token}&units=metric&lang=RU"
+        )
+        data = r.json()
+
+        # Название города получаем из ответа от сервера
+        city = data['name']
+        # Забираем информацию о текущей погоде, содержащейся в ответе от сервера
+        cur_weather = data["main"]["temp"]
+        # Получаем описание погоды из информации, содержащейся в ответе от сервера
+        weather_description = data["weather"][0]["main"]
+        # Если описание погоды совпадает со значением ключа словаря с эмоджи, то выводим значение словаря
+        if weather_description in code_to_smile:
+            wd = code_to_smile[weather_description]
+        else:
+            wd = "Посмотри в окно, не пойму что там за погода!"
+        # Забираем информацию о влажности в ответе от сервера
+        humidity = data["main"]["humidity"]
+        # Забираем информацию о давлении в ответе от сервера
+        pressure = data["main"]["pressure"]
+        # Забираем информацию о скорости ветра в ответе от сервера
+        wind = data["wind"]["speed"]
+        # Время рассвета, высчитанное с учётом timestamp UNIX (с 1970 года - года выхода UNIX)
+        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+        # Время заката, высчитанное с учётом timestamp UNIX (с 1970 года - года выхода UNIX)
+        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+        # Продолжительность светового дня
+        length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
+            data["sys"]["sunrise"]
+        )
+        await message.reply(f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
+              f"Погода в городе: {city}\nТемпература: {cur_weather}°C {wd}\n"
+              f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/сек.\n"
+              f"Восход солнца: {sunrise_timestamp.strftime('%Y-%m-%d %H:%M')}\nЗакат солнца: {sunset_timestamp.strftime('%Y-%m-%d %H:%M')}\nПродолжительность дня: {length_of_the_day}\n"
+              f"Хорошего дня!")
+    except:
+        await message.reply("\U00002620 Проверьте название города\U00002620")
+
 if __name__ == '__main__':
     executor.start_polling(dp)
