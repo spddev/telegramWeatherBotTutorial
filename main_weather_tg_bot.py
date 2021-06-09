8abd983f3d2771b9faa96e79179bcaf21b79046c
SpdDev <spd@tutanota.com>
2021-06-09 16:59:09 +0400

    Add buttons for more user interactions

--- main_weather_tg_bot.py
+++ main_weather_tg_bot.py
@@ -11,8 +11,23 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=["start"])
 async def start_command(message: types.Message):
-    await message.reply("Привет! Напиши мне название города и я пришлю сводку погоды!")
+    # Отображение разметки клавиатуры
+    keyboard_markup = types.InlineKeyboardMarkup(row_width=3)
+    button_show_weather = types.InlineKeyboardButton(text="Показать сводку погоды",callback_data='show_weather')
+    keyboard_markup.add(button_show_weather)
+    await message.reply("Привет! Для запроса сводки погоды нажми кнопку ниже",reply_markup=keyboard_markup)
 
+
+@dp.callback_query_handler(text=['show_weather','yes', 'no'])
+async def inline_kb_answer_callback_handler(query: types.CallbackQuery):
+    answer_data = query.data
+    if answer_data == 'show_weather':
+        text = "Напиши мне название города и я пришлю сводку погоды!"
+    elif answer_data == 'yes':
+        text = "Напиши мне название города и я пришлю сводку погоды!"
+    else:
+        text ="Хорошего дня!"
+    await bot.send_message(query.from_user.id, text)
 
 @dp.message_handler()
 async def get_weather(message: types.Message):
@@ -26,6 +41,10 @@ async def get_weather(message: types.Message):
         "Snow": "Снег \U0001F328",
         "Mist": "Туман \U0001F32B",
     }
+    keyboard_markup = types.InlineKeyboardMarkup(row_width=3)
+    button_again_yes = types.InlineKeyboardButton(text="Да!",callback_data='yes')
+    button_again_no = types.InlineKeyboardButton(text="Нет!",callback_data='no')
+
     try:
         r = requests.get(
             f"http://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_weather_api_token}&units=metric&lang=RU"
@@ -57,11 +76,14 @@ async def get_weather(message: types.Message):
         length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
             data["sys"]["sunrise"]
         )
+
         await message.reply(f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
               f"Погода в городе: {city}\nТемпература: {cur_weather}°C {wd}\n"
               f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/сек.\n"
-              f"Восход солнца: {sunrise_timestamp.strftime('%Y-%m-%d %H:%M')}\nЗакат солнца: {sunset_timestamp.strftime('%Y-%m-%d %H:%M')}\nПродолжительность дня: {length_of_the_day}\n"
-              f"Хорошего дня!")
+              f"Восход солнца: {sunrise_timestamp.strftime('%Y-%m-%d %H:%M')}\nЗакат солнца: {sunset_timestamp.strftime('%Y-%m-%d %H:%M')}\nПродолжительность дня: {length_of_the_day}\n")
+        keyboard_markup.add(button_again_yes, button_again_no)
+        await message.reply("Запросить сводку погоды снова?",reply_markup=keyboard_markup)
+
     except:
         await message.reply("\U00002620 Проверьте название города\U00002620")
 
